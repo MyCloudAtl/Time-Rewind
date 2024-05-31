995213733f42149836442d884661e60d57a3dd38
MyCloudAtl <megandmashburn@gmail.com>
2024-05-31 17:55:13 -0400

    pumping up the seeds

--- seed/songs.js
+++ seed/songs.js
@@ -4,15 +4,15 @@ const { Year, Music } = require('../models')
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 const main = async () => {
-    const genxB = await Year.find({ name:'Generation X (Begin 1965-1970)'})
-    const genxM = await Year.find({ name:'Generation X (Mid 1970-1975)'})
-    const genxE = await Year.find({ name:'Generation X (End 1975-1980)'})
-    const millB = await Year.find({ name:'Millennial (Begin 1981-1986)'})
-    const millM = await Year.find({ name:'Millennial (Mid 1986-1991)'})
-    const millE = await Year.find({ name:'Millennial (End 1991-1996)'})
-    const genzB = await Year.find({ name:'Generation Z (Begin 1997-2002)'})
-    const genzM = await Year.find({ name:'Generation Z (Mid 2002-2007)'})
-    const genzE = await Year.find({ name:'Generation Z (End 2007-2012)'})
+    const genxB = await Year.find({ year:'Generation X (Begin 1965-1970)'})
+    const genxM = await Year.find({ year:'Generation X (Mid 1970-1975)'})
+    const genxE = await Year.find({ year:'Generation X (End 1975-1980)'})
+    const millB = await Year.find({ year:'Millennial (Begin 1981-1986)'})
+    const millM = await Year.find({ year:'Millennial (Mid 1986-1991)'})
+    const millE = await Year.find({ year:'Millennial (End 1991-1996)'})
+    const genzB = await Year.find({ year:'Generation Z (Begin 1997-2002)'})
+    const genzM = await Year.find({ year:'Generation Z (Mid 2002-2007)'})
+    const genzE = await Year.find({ year:'Generation Z (End 2007-2012)'})
 
   const songs = [
     {
